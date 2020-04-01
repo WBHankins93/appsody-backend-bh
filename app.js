2e8d07a37f488c82995477d9a3ccff614840ee43
Ben Hankins <32532133+WBHankins93@users.noreply.github.com>
2020-04-01 14:26:30 -0500

    Edited console.log

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(cookieParser());
 var registrants = {};
 
 app.get('/', function (req, res) {
-  res.send('backend api is up')
+  res.send('backend api is up and running. Have fun!')
 });
 
 app.post('/signup', function (req, res) {
